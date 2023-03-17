3aa15be500550cc4b2b935650b478e36fc06bf4a
Benjamin Hall <hallb1016@gmail.com>
2023-03-17 19:04:26 -0400

    Add missing documentation

--- waveform_cfg.h
+++ waveform_cfg.h
@@ -9,6 +9,7 @@
 
 #include <stdint.h>
 
+/** Waveform configuration parameter types. */
 typedef enum _waveform_cfg_param_t {
 	PARAM_RECV,
 	PARAM_AMPLITUDE,
@@ -17,7 +18,10 @@ typedef enum _waveform_cfg_param_t {
 	PARAM_ENABLE,
 } waveform_cfg_param_t;
 
+/** Represents a waveform configuration value. */
 typedef struct _waveform_cfg_t {
+	/** The parameter type */
 	waveform_cfg_param_t type;
+	/** The new value */
 	uint32_t value;
 } waveform_cfg_t;
